368f056d897331501b68f791cd13f40ad70d00d8
Eduardo Passos <edupassosa18@gmail.com>
2026-06-15 22:19:10 -0300

    Estilização minima do mapa feita

--- frontend/src/map/LocationsLayer.tsx
+++ frontend/src/map/LocationsLayer.tsx
@@ -1,25 +1,29 @@
 import { Marker, Popup } from "react-leaflet";
-import type { Location } from "../types/location";
+import type { Location } from "../types/Location";
+import MapPopup from "./MapPopup";
 
 type Props = {
     locations: Location[];
+    onSelectLocation: (loc: Location) => void;
 };
 
-export default function LocationsLayer({ locations }: Props) {
+export default function LocationsLayer({ locations, onSelectLocation }: Props) {
     return (
         <>
             {locations.map((loc) => (
                 <Marker
                     key={loc.location_id}
-                    position={[
-                        loc.coordinate[0],
-                        loc.coordinate[1],
-                    ]}
+                    position={[loc.coordinate[0], loc.coordinate[1]]}
+                    eventHandlers={{
+                        click: () => onSelectLocation(loc),
+                    }}
                 >
                     <Popup>
-                        <h3>{loc.name}</h3>
-                        <p>{loc.description}</p>
-                        {loc.type && <small>Type: {loc.type}</small>}
+                        <MapPopup
+                            title={loc.name}
+                            description={loc.description}
+                            subtitle={loc.type}
+                        />
                     </Popup>
                 </Marker>
             ))}
