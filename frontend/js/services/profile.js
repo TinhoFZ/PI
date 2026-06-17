55512f516cba33a0b3a1a22936752d94bec67ccf
TinhoFZ <edupassosa18@gmail.com>
2026-06-17 09:24:11 -0300

    CSS de páginas adicionado, mapa funcional, perfil coleta dados

---
+++ frontend/js/services/profile.js
@@ -0,0 +1,54 @@
+const email = document.querySelector('.profile-email');
+const name = document.querySelector('.profile-name');
+
+const xp = document.querySelector('#xp-text');
+const level = document.querySelector('#level-text');
+const quest = document.querySelector('#quest-text');
+const treasure = document.querySelector('#treasure-text');
+
+async function loadProfile() {
+
+    try {
+        const token = localStorage.getItem('token');
+
+        const userResponse =       
+            await fetch(
+                `${API_URL}/me`,
+                {
+                    method: "GET",
+                    headers: {
+                        authorization: `Bearer ${token}`
+                    }
+                }
+            )
+    
+        const questResponse =
+            await fetch(
+                `${API_URL}/quests/my-quests`,
+                {
+                    headers: {
+                        authorization: `Bearer ${token}`
+                    }
+                }
+            )
+
+        const data = [await userResponse.json(), await questResponse.json()];
+
+        if (!userResponse.ok && questResponse.ok) {
+
+            alert(
+                data.message || "Erro ao carregar perfil"
+            )
+
+            return;
+        }
+
+        email.innerText = data[0].email;
+        name.innerText = data[0].name;
+        xp.children[0].innerText = data[0].xp;
+        level.children[0].innerText = data[0].level;
+
+    } catch(error) {
+        console.error(error);
+    }
+}
